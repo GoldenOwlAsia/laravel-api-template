ea45deb6559a4a5a3f30ae86f242b9da1d95c089
Tonio <tonio.bui.goldenowl@gmail.com>
2023-04-20 11:06:05 +0700

    Update Docker & update code

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -11,6 +11,15 @@ use Illuminate\Support\Facades\Route;
 class RouteServiceProvider extends ServiceProvider
 {
     /**
+     * This namespace is applied to your controller routes.
+     *
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'App\Http\Controllers';
+
+    /**
      * The path to your application's "home" route.
      *
      * Typically, users are redirected here after authentication.
@@ -29,9 +38,11 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
--- src/routes/api.php
+++ src/routes/api.php
@@ -19,5 +19,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::namespace('API\V1')->group(function () {
-    Route::resource('users', 'UserController');
+    Route::resource('users', UserController::class);
 });
+// Route::resource('users', 'UserController');
